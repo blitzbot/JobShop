d0ac3c611d1b6f1011d16baa81af31681001e4e4
blitzbot <franciscodua@gmail.com>
2015-05-19 12:35:40 +0100

    O Fred nao faz nada

--- job_shop_solution.lisp
+++ job_shop_solution.lisp
@@ -1,4 +1,15 @@
 ;(in-package :user)
 
-(defun iterative-probing
-)
+(defun iterative-probing (problema)
+	(let ((objectivo? (problema-objectivo? problema)))
+		;procura sonda
+		(labels ((procura-sonda (estado)
+			(block procura-sonda
+				(if ((funcall objectivo? estado) estado)
+					(let ((sucessores (problema-gera-sucessores problema estado)))
+						(if (null sucessores)
+							nil
+							(return-from procura-sonda (nth (random (length sucessores)) sucessores))))))))
+		(procura-sonda (problema-estado-inicial problema)))))
+
+(defun improved-lds())
